9a56f907fbcd12acd392075f98d212a15fbbdfd9
Gourab Paul <gourabpaul900@gmail.com>
2022-10-01 12:33:04 +0530

    added template1,2,3

--- src/pages/LandingPage/TemplateSection.tsx
+++ src/pages/LandingPage/TemplateSection.tsx
@@ -1,173 +1,14 @@
 import React, { FC } from "react";
 import template_1 from "../../static/images/templates/template_1.png";
 import template_2 from "../../static/images/templates/template_2.png";
-
-const template2 = [
-  {
-    name: "about1",
-    x: 0,
-    y: 0,
-    w: 8,
-    h: 8,
-    data: {
-      name: "",
-      profession: "",
-      about: "",
-      style: {
-        bgColor: "#ffffff",
-        textColor: "#000000",
-      },
-    },
-  },
-  {
-    name: "contact1",
-    x: 8,
-    y: 0,
-    w: 4,
-    h: 8,
-    data: {
-      title: "",
-      flipped: false,
-      data: {
-        address: [""],
-        emails: [""],
-        phno: "",
-      },
-      style: {
-        bgColor: "#ffffff",
-        textColor: "#000000",
-      },
-    },
-  },
-  {
-    name: "educations1",
-    x: 0,
-    y: 8,
-    w: 8,
-    h: 11,
-    data: {
-      title: "",
-      data: [
-        {
-          id: "education1663996298146",
-          courseName: "",
-          courseResults: "",
-          organizationName: "",
-          courseDuration: "",
-        },
-      ],
-      style: {
-        bgColor: "#ffffff",
-        textColor: "#000000",
-      },
-    },
-  },
-  {
-    name: "skills1",
-    x: 8,
-    y: 8,
-    w: 4,
-    h: 13,
-    data: {
-      color: "#123456",
-      title: "",
-      chipRadius: 10,
-      chipSize: 3,
-      filled: true,
-      flipped: false,
-      data: [],
-      style: {
-        bgColor: "#ffffff",
-        textColor: "#000000",
-      },
-    },
-  },
-  {
-    name: "works1",
-    x: 0,
-    y: 19,
-    w: 8,
-    h: 12,
-    data: {
-      title: "",
-      data: [
-        {
-          id: "work1663996298146",
-          workOrganizationName: "",
-          workLocation: "",
-          jobTitle: "",
-          workDetails: [""],
-          workDuration: "",
-        },
-      ],
-      style: {
-        bgColor: "#ffffff",
-        textColor: "#000000",
-      },
-    },
-  },
-  {
-    name: "projects1",
-    x: 0,
-    y: 31,
-    w: 8,
-    h: 15,
-    data: {
-      title: "",
-      data: [
-        {
-          id: "project1663996298146",
-          projectName: "",
-          projectDetails: [""],
-        },
-      ],
-      style: {
-        bgColor: "#ffffff",
-        textColor: "#000000",
-      },
-    },
-  },
-  {
-    name: "others1",
-    x: 8,
-    y: 42,
-    w: 4,
-    h: 11,
-    data: {
-      title: "",
-      bullet: -1,
-      data: [""],
-      style: {
-        bgColor: "#ffffff",
-        textColor: "#000000",
-      },
-    },
-  },
-  {
-    name: "ratings2",
-    x: 8,
-    y: 21,
-    w: 4,
-    h: 11,
-    data: {
-      title: "",
-      ratingType: "star",
-      icon: "circle",
-      flipped: false,
-      data: [],
-      style: {
-        bgColor: "#ffffff",
-        textColor: "#000000",
-      },
-    },
-  },
-];
+import template_3 from "../../static/images/templates/template_3.jpg";
+import { template1, template2, template3 } from "./TemplateData";
 
 const templates = [
   {
     templateTitle: "Template one",
     templateImage: template_1,
-    templateData: template2,
+    templateData: template1,
   },
   {
     templateTitle: "Template Two",
@@ -176,8 +17,8 @@ const templates = [
   },
   {
     templateTitle: "Template Three",
-    templateImage: template_1,
-    templateData: template2,
+    templateImage: template_3,
+    templateData: template3,
   },
 ];
 
